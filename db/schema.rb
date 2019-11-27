5394e2fa6cf7a1e12ab348e18f70d41c28bef20a
beccaelenzil <beccaelenzil@gmail.com>
2019-11-27 10:47:02 -0800

    up to date

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170407164321) do
+ActiveRecord::Schema.define(version: 2019_10_21_180047) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,10 @@ ActiveRecord::Schema.define(version: 20170407164321) do
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "uid"
+    t.string "provider"
+    t.string "name"
+    t.string "email"
   end
 
   create_table "votes", force: :cascade do |t|
@@ -26,8 +30,8 @@ ActiveRecord::Schema.define(version: 20170407164321) do
     t.integer "work_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_votes_on_user_id", using: :btree
-    t.index ["work_id"], name: "index_votes_on_work_id", using: :btree
+    t.index ["user_id"], name: "index_votes_on_user_id"
+    t.index ["work_id"], name: "index_votes_on_work_id"
   end
 
   create_table "works", force: :cascade do |t|
